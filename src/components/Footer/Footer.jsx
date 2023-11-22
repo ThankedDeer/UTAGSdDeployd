1ead82eac02de80b6184e072fd47329570af82dc
ThankedDeer <117323510+ThankedDeer@users.noreply.github.com>
2023-11-22 08:29:15 -0600

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__main-content">
+        <div className="footer__main-content--info">
+          
+          <p className="footer-info-start footer--mainText">
+            Universidad Tecnológica de Aguascalientes
+          </p>
+        </div>
+        <div className="footer__main-content--info">
+          <p className="footer-title">Enlaces</p>
+          <a href="/">Inicio</a>
+          <a href="/TSU">Carreras</a>
+          <a href="/Becas">Becas</a>
+          <a href="/Estadias">Estadias</a>
+          {/* Agrega otros enlaces relevantes de la universidad */}
+        </div>
+        <div className="footer__main-content--info">
+          <p className="footer-title">Horario de atención</p>
+          <p className="footer-info">Lunes a Viernes: 8:00 am - 6:00 pm</p>
+          <p className="footer-info">Sábado: 9:00 am - 2:00 pm</p>
+          <p className="footer-info">Domingo: Cerrado</p>
+        </div>
+        <div className="footer__main-content--info">
+          <p className="footer-title">Contacto</p>
+          <a href="https://www.uta.edu.mx" target="_blank" rel="noopener noreferrer">
+            <span className="footer-loc">
+              Dirección: Dirección de la universidad, Aguascalientes, México
+            </span>{" "}
+          </a>
+          <a href="tel:+123456789" className="footer-info">
+            Teléfono: +123456789
+          </a>
+        </div>
+        <div className="footer__mainContent--info mt-3 mt-lg-0">
+          <p className="footer-title">Redes Sociales</p>
+          <a href="https://www.facebook.com/TuPaginaDeFacebook" target="_blank">Facebook</a>
+          <a href="https://twitter.com/TuCuentaDeTwitter" target="_blank">Twitter</a>
+          {/* Agrega otros enlaces a las redes sociales de la universidad */}
+        </div>
+      </div>
+    </footer>
+  );
+}
