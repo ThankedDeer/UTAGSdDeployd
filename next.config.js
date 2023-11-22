d68485db5d6b62c2060cc49242a25272890fb884
ThankedDeer <117323510+ThankedDeer@users.noreply.github.com>
2023-11-22 08:44:44 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-
+  output: 'export', // Configuración para la exportación estática
+  // Otras configuraciones pueden ir aquí...
 };
 
 module.exports = nextConfig;
